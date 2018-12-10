fe221c05387d98151f4cbeeeee8ccfa7872bbe6a
EDUARDO MENDOZA <rl238513@rlas-macair-36.local>
2018-12-10 11:05:29 -0600

    idk

--- Rock,Paper,Scissors/ViewController.swift
+++ Rock,Paper,Scissors/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var winnerImage: UIImageView!
     @IBOutlet weak var backgroundImmage: UIImageView!
     
+    var pictureArray = [#imageLiteral(resourceName: "paper.png"), #imageLiteral(resourceName: "rock.png"), #imageLiteral(resourceName: "scissors.png")]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,5 +29,15 @@ class ViewController: UIViewController {
     
     @IBAction func scissorButton(_ sender: Any) {
     }
+    
+    func systemPicture {
+        systemChoice.image = pictureArray [randomInt]
+    }
+    
+    let randomInt = Int.random(in: 0..<3)
+    
+    
+    
+    
     
 }
